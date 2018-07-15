e17f9284b0ba012d69fd788f23bd11ac399d365e
Saul van der Walt <saulvdw@gmail.com>
2018-07-15 12:06:44 +0200

    22 lines, nothing lost. micro? very much so!

--- dataGun.mjs
+++ dataGun.mjs
@@ -1,32 +1,22 @@
-const def = o => o !== undefined
-const listMap = (map = new Map()) => Object.assign(
-  (key, val) => def(val) ? (map.has(key) ? map : map.set(key, new Set())).get(key).add(val) : map.get(key),
-  {
-    map,
-    del (key, val) {
-      map.has(key) && map.get(key).delete(val).size < 1 && map.delete(key)
-    },
-    has (key, val) {
-      const list = map.get(key)
-      return def(val) ? list && list.has(val) : !!list
-    },
-    each (key, fn) { map.has(key) && map.get(key).forEach(fn) }
-  }
-)
-
-const dataGun = (listeners = listMap()) => Object.assign((evt, fn, one) => {
+const callAsync = this.requestIdleCallback || this.requestIdleCallback || this.setImmediate || setTimeout
+const dataGun = (listeners = dataGun.listMap()) => Object.assign((evt, fn, one) => {
   fn._one = one
-  fn.stop = () => listeners.del(evt, fn)
+  fn.stop = listeners.del.bind(null, evt, fn)
   listeners(evt, fn)
   return fn
 }, {
   listeners,
   fire (evt, ...data) {
-    listeners.each(evt, h => {
-      setTimeout(() => h(...data), 0)
-      h._one && h.stop()
-    })
+    callAsync(() => listeners.each(evt, h => {
+      h(...data)
+      if (h._one) h.stop()
+    }))
   }
 })
-dataGun.listMap = listMap
+dataGun.listMap = (map = new Map()) => Object.assign((key, val) => val != null
+  ? (map.has(key) ? map : map.set(key, new Set())).get(key).add(val) : map.get(key), {
+  del: (key, val) => (map.has(key) && map.get(key).delete(val).size < 1 && map.delete(key), val),
+  has: (key, val) => map.has(key) && (val == null || map.get(key).has(val)),
+  each (key, fn) { map.has(key) && map.get(key).forEach(fn) }
+})
 export default dataGun
